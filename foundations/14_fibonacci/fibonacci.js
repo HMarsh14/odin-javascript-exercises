769b54728c197e96cd26e3bec8c3dea8a294bf38
HMarsh1409 <hmarsh1409@gmail.com>
2026-09-11 13:48:36 +0100

    feat: add fibonacci function to return nth number

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    const n = parseInt(num);
 
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+
+    const fib = [0, 1];
+
+    for (let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[n];
 };
 
 // Do not edit below this line
