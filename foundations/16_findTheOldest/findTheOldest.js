80d6b1acc240d14ceb6dd54ad6c2b7861672466b
HMarsh1409 <hmarsh1409@gmail.com>
2026-09-11 14:10:31 +0100

    feat: complete findTheOldest challenge

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
-// Do not edit below this line
+const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath,
+        );
+
+        if (currentAge > oldestAge) {
+            return currentPerson;
+        } else {
+            return oldest;
+        }
+    });
+};
+
 module.exports = findTheOldest;
